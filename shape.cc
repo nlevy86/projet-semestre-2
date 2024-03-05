a2cd638ce6891d595f59477be4b6fc7e4bd7f3a0
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-05 19:12:48 +0100

    Update shape.cc

--- shape.cc
+++ shape.cc
@@ -9,14 +9,16 @@ constexpr double epsil_zero(0.5) ;
 using namespace std;
 
 void shape::setbase(abs,ord){
-  if ((ab>=1 and ab<=max-1) and (or>=1 and or<=max-1){
-    S2d.x=ab;
-    S2d.y=or;
-  } else { 
+  if ((abs>=1 and abs<=max-1) and (ord>=1 and ord<=max-1){
+    S2d.x=abs;
+    S2d.y=ord;
+  } else { lifeform_center_outside(abs, ord);}
 };
 
 void shape::setangle(rad){
-  angle=rad;
+  if (rad<=M_PI and rad>=M_PI){
+    angle=rad;
+  }else{
 };
 
 void shape::setlongueur(taille){
