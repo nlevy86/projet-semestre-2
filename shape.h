18b21d01a2d49120d254a9003bbf9730a4a1449c
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-05 19:17:50 +0100

    Update shape.h

--- shape.h
+++ shape.h
@@ -5,8 +5,6 @@ struct S2d {double x=0; doube y=0;};
 class Segment {
 public:
   void setbase (double abs, double ord);
-  void setangle();
-  void setlongueur();
 private:
   S2d base;
   double angle;
