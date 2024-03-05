6e187c31d7553eafc21d5cffa70b9b6a96da70aa
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-05 20:29:53 +0100

    Update simulation.cc

--- simulation.cc
+++ simulation.cc
@@ -8,5 +8,9 @@
 string lecture(){
 
 
-string testage(age){
-  if (age<0) {
+void testage(age){
+  if (age<0) {string lifeform_age};
+}
+
+void testlongueur(longueur){
+  if (longueur<(l_repro-l_seg_interne) or longueur>=l_repro){
