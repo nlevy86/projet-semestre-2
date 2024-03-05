0e80652d0bd297bcb152613f42f1d1237a48053f
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-05 17:19:25 +0100

    Update shape.cc

--- shape.cc
+++ shape.cc
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-void shape::setS2d(ab,or){
-  S2d.x=ab;
-  S2d.y=or;
+void shape::setbase(ab,or){
+  if ((ab>=1 and ab<=max-1) and (or>=1 and or<=max-1){
+    S2d.x=ab;
+    S2d.y=or;
+  } else { 
 };
 
 void shape::setangle(rad){
