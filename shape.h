e45a60323d1ee1696b29ba7080579c87c3459057
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-18 18:24:32 +0100

    Update shape.h

--- shape.h
+++ shape.h
@@ -16,3 +16,7 @@ struct segment{
 };
 
 double ecart_angulaire(segment segment_1, segment segment_2);
+bool superpo_commun(segment segment_1, segment segment_2);
+bool onSegment(S2d p, S2d q, S2d r);
+int orientation(S2d p, S2d q, S2d r);
+bool doIntersect(S2d p1, S2d q1, S2d p2, S2d q2);
