418d39050bcc95066d69816285798c4cee41c160
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-21 16:05:01 +0100

    Update lifeform.cc

--- lifeform.cc
+++ lifeform.cc
@@ -23,11 +23,11 @@ bool positive_age(int age){
 }
 
 bool Corail::corail_in(vector<Segment> seg){
-	for (int i(0); i<= this->nb_segment-1; ++i){
+	for (int i(0); i<= nb_segment-1; ++i){
 		if (seg[i].base.x < 1 or seg[i].base.x > dmax-1 or 
 		seg[i].base.y < 1 or seg[i].base.y > dmax-1){
 			cout << 
-			message::lifeform_computed_outside(this->id, seg[i].base.x, seg[i].base.y);
+			message::lifeform_computed_outside(id, seg[i].base.x, seg[i].base.y);
 			exit(EXIT_FAILURE);
 		}
 		S2d fin;
@@ -35,7 +35,7 @@ bool Corail::corail_in(vector<Segment> seg){
 		fin.y = seg[i].base.y + seg[i].longueur * sin(seg[i].angle);
 		if (fin.x < 1 or fin.x > dmax-1 or fin.y < 1 or fin.y > dmax-1){
 			cout << 
-			message::lifeform_computed_outside(this->id, seg[i].base.x, seg[i].base.y);
+			message::lifeform_computed_outside(id, seg[i].base.x, seg[i].base.y);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -43,9 +43,9 @@ bool Corail::corail_in(vector<Segment> seg){
 }
 
 bool Corail::segment_length_in(vector<Segment> seg){
-	for (int i(0); i<= this->nb_segment-1; ++i){
+	for (int i(0); i<= nb_segment-1; ++i){
 		if (seg[i].longueur >= l_repro or seg[i].longueur < l_repro - l_seg_interne){
-			message::segment_length_outside(this->id, seg[i].longueur);
+			message::segment_length_outside(id, seg[i].longueur);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -53,9 +53,9 @@ bool Corail::segment_length_in(vector<Segment> seg){
 }
 
 bool Corail::segment_angle_in(vector<Segment> seg){
-	for (int i(0); i<= this->nb_segment-1; ++i){
+	for (int i(0); i<= nb_segment-1; ++i){
 		if (seg[i].angle > M_PI or seg[i].longueur < -M_PI){
-			message::segment_angle_outside(this->id, seg[i].angle);
+			message::segment_angle_outside(id, seg[i].angle);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -63,9 +63,9 @@ bool Corail::segment_angle_in(vector<Segment> seg){
 }
 
 bool Corail::segment_not_superpo(vector<Segment> seg){
-	for (int i(0); i<= this->nb_segment-1; ++i){
+	for (int i(0); i<= nb_segment-1; ++i){
 		if (superpo_commun(seg[i],seg[i+1])){
-			message::segment_superposition(this->id, i, i+1);
+			message::segment_superposition(id, i, i+1);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -73,9 +73,9 @@ bool Corail::segment_not_superpo(vector<Segment> seg){
 }
 
 bool Corail::segment_not_coll_him(vector<Segment> seg){
-	if (this->nb_segment < 2){return true;}
-	for (int i(0); i<= this->nb_segment-3; ++i){
-		for (int j(i+2); j<= this->nb_segment-1; ++j){
+	if (nb_segment < 2){return true;}
+	for (int i(0); i<= nb_segment-3; ++i){
+		for (int j(i+2); j<= nb_segment-1; ++j){
 			S2d fin_i;
 			fin_i.x = seg[i].base.x + seg[i].longueur * cos(seg[i].angle);
 			fin_i.y = seg[i].base.y + seg[i].longueur * sin(seg[i].angle);
@@ -83,7 +83,7 @@ bool Corail::segment_not_coll_him(vector<Segment> seg){
 			fin_j.x = seg[j].base.x + seg[j].longueur * cos(seg[j].angle);
 			fin_j.y = seg[j].base.y + seg[j].longueur * sin(seg[j].angle);
 			if (doIntersect(seg[i].base, fin_i, seg[j].base, fin_j)){
-				message::segment_collision(this->id, i, this->id, j);;
+				message::segment_collision(id, i, id, j);;
 				exit(EXIT_FAILURE);
 			}
 		}
