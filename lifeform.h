ef3b298742250715426b6d5ed2513aa989617ef5
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-21 15:58:20 +0100

    Update lifeform.h

--- lifeform.h
+++ lifeform.h
@@ -10,22 +10,22 @@ class Algue{
 
 public:
 	bool lifeform_in(S2d position); 
-	bool positive_age(double age);
+	bool positive_age(int age);
 private:
 	S2d pos_algue;
-	double age_algue;
+	int age_algue;
 };
 
-class corail{
+class Corail{
 public:
 	bool corail_in(std::vector<Segment> seg);
-	bool positive_age(double age);
+	bool positive_age(int age);
 	bool segment_length_in(std::vector<Segment> seg);
 	bool segment_angle_in(std::vector<Segment> seg);
 	bool segment_not_superpo(std::vector<Segment> seg);
 	bool segment_not_coll_him(std::vector<Segment> seg);
 private:
-	double age_corail;
+	int age_corail;
 	unsigned int id;
 	Status_cor status_corail;
 	Dir_rot_cor dir_rot_corail;
@@ -39,11 +39,11 @@ class Sca{
 
 public:
 	bool lifeform_in(S2d position); 
-	bool positive_age(double age);
+	bool positive_age(int age);
 	bool ray_in(double ray);
 private:
 	S2d pos_sca;
-	double age_sca;
+	int age_sca;
 	double ray_sca;
 	Status_sca status_scaven;
 	unsigned int cor_id_cible;
