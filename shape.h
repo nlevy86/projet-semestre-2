7ab844cb584aa12231ba679f96c6c1ad9f31987c
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-21 19:20:08 +0100

    Add files via upload

---
+++ shape.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+constexpr double epsil_zero(0.5) ;
+
+struct S2d{
+	double x=0.;
+	double y=0.;
+};
+
+struct Segment{
+	S2d base;
+	double angle;
+	double longueur;
+};
+
+double ecart_angulaire(Segment segment_1, Segment segment_2);
+bool superpo_commun(Segment segment_1, Segment segment_2, bool is_epsil_zero);
+bool on_segment(S2d p, S2d q, S2d r, bool is_epsil_zero);
+int orientation(S2d p, S2d q, S2d r, bool is_epsil_zero);
+bool do_intersect(S2d p1, S2d q1, S2d p2, S2d q2, bool is_epsil_zero);
