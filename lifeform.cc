74a29af2d156eafd17c4fa104e19c580f8816e3a
nlevy86 <154896659+nlevy86@users.noreply.github.com>
2024-03-24 15:53:23 +0100

    Update lifeform.cc

--- lifeform.cc
+++ lifeform.cc
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-bool lifeform_in(S2d position){
+const bool lifeform_in(S2d position){
 	if (position.x < 1 or position.x > dmax-1 or 
 		position.y < 1 or position.y > dmax-1){
 		cout << message::lifeform_center_outside(position.x, position.y);
@@ -14,7 +14,7 @@ bool lifeform_in(S2d position){
 	return true;
 }
 
-bool positive_age(int age){
+const bool positive_age(int age){
 	if (age>0){
 		return true;
 	}
@@ -22,7 +22,7 @@ bool positive_age(int age){
 	exit(EXIT_FAILURE);
 }
 
-bool Corail::corail_in(vector<Segment> seg){
+const bool Corail::corail_in(vector<Segment> seg){
 	for (int i(0); i<= nb_segment-1; ++i){
 		S2d fin;
 		fin.x = seg[i].base.x + seg[i].longueur * cos(seg[i].angle);
@@ -39,7 +39,7 @@ bool Corail::corail_in(vector<Segment> seg){
 	return true;
 }
 
-bool Corail::segment_length_in(vector<Segment> seg){
+const bool Corail::segment_length_in(vector<Segment> seg){
 	for (int i(0); i<= nb_segment-1; ++i){
 		if (seg[i].longueur >= l_repro or seg[i].longueur < l_repro - l_seg_interne){
 			message::segment_length_outside(id, seg[i].longueur);
@@ -49,7 +49,7 @@ bool Corail::segment_length_in(vector<Segment> seg){
 	return true;
 }
 
-bool Corail::segment_angle_in(vector<Segment> seg){
+const bool Corail::segment_angle_in(vector<Segment> seg){
 	for (int i(0); i<= nb_segment-1; ++i){
 		if (seg[i].angle > M_PI or seg[i].longueur < -M_PI){
 			message::segment_angle_outside(id, seg[i].angle);
@@ -59,7 +59,7 @@ bool Corail::segment_angle_in(vector<Segment> seg){
 	return true;
 }
 
-bool Corail::segment_not_superpo(vector<Segment> seg, bool is_epsil_zero){
+const bool Corail::segment_not_superpo(vector<Segment> seg, bool is_epsil_zero){
 	for (int i(0); i<= nb_segment-1; ++i){
 		if (superpo_commun(seg[i],seg[i+1], is_epsil_zero)){
 			message::segment_superposition(id, i, i+1);
@@ -69,7 +69,7 @@ bool Corail::segment_not_superpo(vector<Segment> seg, bool is_epsil_zero){
 	return true;
 }
 
-bool Corail::segment_not_coll_him(vector<Segment> seg, bool is_epsil_zero){
+const bool Corail::segment_not_coll_him(vector<Segment> seg, bool is_epsil_zero){
 	if (nb_segment < 2){return true;}
 	for (int i(0); i<= nb_segment-3; ++i){
 		for (int j(i+2); j<= nb_segment-1; ++j){
@@ -102,7 +102,7 @@ void Corail::add_segment(double angle, double length) {
 	cor.emplace_back(S2d{x, y}, angle, length);
 }
 
-bool Sca::ray_in(double ray){
+const bool Sca::ray_in(double ray){
 	if (ray>=r_sca and ray<r_sca_repro){
 		return true;
 	}
